98760ca2459d84383b64baa91423f89a1a21200b
David Tkeshelashvili <54507573+tkesho@users.noreply.github.com>
2023-06-25 13:49:50 +0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,20 @@
+from google.cloud import vision
+import io
+
+def extract_text(image_path):
+    client = vision.ImageAnnotatorClient.from_service_account_json(r'C:\Users\dtkes\Downloads\CNN_AlphabetRecognition-master\grab text\secret-lambda-390612-5d20899e8bad.json')
+    
+    with io.open(image_path, 'rb') as image_file:
+        content = image_file.read()
+    
+    image = vision.Image(content=content)
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    
+    if texts:
+        extracted_text = texts[0].description
+        return extracted_text
+    else:
+        return None
+
+ 
